0cde384820936565d110f313e207451d6cfea49c
Akma <akalmanbay@gmail.com>
2022-11-29 09:56:19 +0600

    Add key

--- src/App.js
+++ src/App.js
@@ -9,26 +9,26 @@ import ContentComponent from './ContentComponent';
 function App() {
   let articlesInfo = [
     {
-      title: 'Title1',
-      description: 'Description1',
+      title: '7 Practical CSS Tips',
+      description: 'You not only learn more Python by implementing what you already know but, in the end, you can see how all your hard work pays off.',
       img: 'Img.png'
     },
     {
-      title: 'Title2',
-      description: 'Description2',
+      title: '7 Practical CSS Tips',
+      description: 'Recently, I’ve been automating tasks more than often due to my lack of time. Thanks to that I have 5 new projects that I classified as beginner, intermediate, and advanced. You’ll find links to the full scripts and tutorials to solve each project. Also, I’m leaving a challenge to each of them to test your Python skills.To make things easier, I already created a template for a cover letter. Here’s how the template we’ll use looks...',
       img: 'Img-5.png'
     },
     {
-      title: 'Title3',
-      description: 'Description3',
+      title: '7 Practical CSS Tips',
+      description: 'Recently, I’ve been automating tasks more than often due to my lack of time. Thanks to that I have 5 new projects that I classified as beginner, intermediate, and advanced. You’ll find links to the full scripts and tutorials to solve each project. Also, I’m leaving a challenge to each of them to test your Python skills.To make things easier, I already created a template for a cover letter. Here’s how the template we’ll use looks...',
       img: 'Img-6.png'
     }
   ]
   return (
     <div className="App" id="container1">
       <Header />
-      {articlesInfo.map((articleInfo) => {
-        return <ContentComponent info={articleInfo} />
+      {articlesInfo.map((articleInfo, index) => {
+        return <ContentComponent info={articleInfo} key={index} />
       })}
 
 
